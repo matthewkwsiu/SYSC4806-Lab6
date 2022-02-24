a304f0de62bd4468b1cb2e39931ed7979db71f16
matthewkwsiu <22455036+matthewkwsiu@users.noreply.github.com>
2022-02-24 15:11:08 -0500

    third commit

---
+++ src/test/java/webtesting/HttpRequestTest.java
@@ -0,0 +1,49 @@
+package webtesting;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+
+
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).contains("Hello, World");
+    }
+
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Test
+//    public void shouldReturnDefaultMessage() throws Exception {
+//        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+//                .andExpect(content().string(containsString("Hello, World")));
+//    }
+}
